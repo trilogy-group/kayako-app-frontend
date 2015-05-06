22461be8dd730cedd0c73e1832ddf41e47653e40
Travis CI <travis@kayako.com>
2015-05-06 13:41:47 +0000

    Build 40

--- assets/admin/assets/vendor.js
+++ assets/admin/assets/vendor.js
@@ -1,6 +1,6 @@
 
 
-Ember.COMPONENT_CSS_LOOKUP = {"ko-breadcrumbs":"ko-breadcrumbs-411bfa27","ko-case-select-field":"ko-case-select-field-948743e9","ko-cases-list":"ko-cases-list-d53a92a8","ko-context-modal":"ko-context-modal-f3b29920","ko-editable-text":"ko-editable-text-3dc53d16","ko-feed":"ko-feed-8ae2526d","ko-page-container":"ko-page-container-353e5816","ko-search":"ko-search-541d22f8","ko-sidebar":"ko-sidebar-8adfbc1f","login":"login-7eee1c54","session/cases/index":"session--cases--index-73c1e88e","session/showcase":"session--showcase-2dde9985"};
+Ember.COMPONENT_CSS_LOOKUP = {"ko-cases-list":"ko-cases-list-63fcb27c","ko-context-modal":"ko-context-modal-c8101ad7","ko-editable-text":"ko-editable-text-888f9d50","ko-feed":"ko-feed-417357bf","ko-pagination":"ko-pagination-80b75ec5","ko-search":"ko-search-95c4cef0","ko-sidebar":"ko-sidebar-f81523f0","ko-tabs":"ko-tabs-cc8af8bb","login":"login-7ab7016f","session/cases/index":"session--cases--index-6e9a4377","session/showcase":"session--showcase-558d2008"};
 Ember.ComponentLookup.reopen({
   lookupFactory: function(name, container) {
     var Component = this._super(name, container);
