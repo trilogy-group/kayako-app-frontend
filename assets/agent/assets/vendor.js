d1da6158119706a2621a726b702d5eec259f48fd
Travis CI <travis@kayako.com>
2015-05-13 08:29:23 +0000

    Build 52

--- assets/agent/assets/vendor.js
+++ assets/agent/assets/vendor.js
@@ -1,6 +1,6 @@
 
 
-Ember.COMPONENT_CSS_LOOKUP = {"ko-breadcrumbs":"ko-breadcrumbs-40b222da","ko-case-select-field":"ko-case-select-field-44c0076f","ko-cases-list":"ko-cases-list-cd3ddc53","ko-context-modal":"ko-context-modal-d262ea97","ko-datepicker":"ko-datepicker-2b8514f4","ko-editable-text":"ko-editable-text-2301d7d0","ko-feed":"ko-feed-2e98e589","ko-page-container":"ko-page-container-9d010b46","ko-pagination":"ko-pagination-23697890","ko-search":"ko-search-0edfb6cb","ko-sidebar":"ko-sidebar-ea2e67b6","login":"login-72a20b5a","session/cases/index":"session--cases--index-646ca1eb","session/showcase":"session--showcase-5f28baa6"};
+Ember.COMPONENT_CSS_LOOKUP = {"ko-breadcrumbs":"ko-breadcrumbs-f39da234","ko-case-checkbox-field":"ko-case-checkbox-field-2dad602c","ko-case-select-field":"ko-case-select-field-fb628e90","ko-cases-list":"ko-cases-list-69e91439","ko-context-modal":"ko-context-modal-2d41af7e","ko-datepicker":"ko-datepicker-8b4e5f48","ko-editable-text":"ko-editable-text-5d12cad0","ko-feed":"ko-feed-f43040c6","ko-info-bar":"ko-info-bar-480feafc","ko-page-container":"ko-page-container-0cd01174","ko-pagination":"ko-pagination-f92d9746","ko-search":"ko-search-e26886bf","ko-sidebar":"ko-sidebar-82c52183","login":"login-810fc0d4","session/cases/index":"session--cases--index-d9ffbef6","session/showcase":"session--showcase-f3c28339"};
 Ember.ComponentLookup.reopen({
   lookupFactory: function(name, container) {
     var Component = this._super(name, container);
