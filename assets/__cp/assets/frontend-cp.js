1f039ea96caa8d89257f470231b04a0021766f37
Travis CI <travis@kayako.com>
2015-08-20 10:03:41 +0000

    Build 1092

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -29394,7 +29394,7 @@ define('frontend-cp/helpers/ko-get-key', ['exports', 'ember'], function (exports
   'use strict';
 
   exports['default'] = Ember['default'].Handlebars.makeBoundHelper(function (value, key) {
-    return value.get(key);
+    return value[key];
   });
 
 });
@@ -63262,7 +63262,7 @@ catch(err) {
 if (runningTests) {
   require("frontend-cp/tests/test-helper");
 } else {
-  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+6e441c10"});
+  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+51820a18"});
 }
 
 /* jshint ignore:end */
