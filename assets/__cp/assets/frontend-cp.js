87b462b387a1b126a389aa5fd2ae40815660f8f0
Travis CI <travis@kayako.com>
2015-09-14 17:26:21 +0000

    Merge pull request #593 from shivamd001/Feature/TestName
    Update commit message of app-frontendAuthor:Pankaj Garg

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -68773,7 +68773,7 @@ catch(err) {
 if (runningTests) {
   require("frontend-cp/tests/test-helper");
 } else {
-  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+f5ba4f5c"});
+  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+6f1bed35"});
 }
 
 /* jshint ignore:end */
