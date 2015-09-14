b461e8294c0d72c4258ee1cac9d712875b426bee
Travis CI <travis@kayako.com>
2015-09-14 16:52:30 +0000

    Build 1543

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -68773,7 +68773,7 @@ catch(err) {
 if (runningTests) {
   require("frontend-cp/tests/test-helper");
 } else {
-  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+d075df74"});
+  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+f5ba4f5c"});
 }
 
 /* jshint ignore:end */
