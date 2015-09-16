e95ea00abb667012ddaa312b992b30ecfe3b7661
Travis CI <travis@kayako.com>
2015-09-16 17:40:37 +0000

    Merge pull request #605 from anthillape/kill-deprecation-warnings
    [deprecations] added more deprecations to deprecation-workflow to be … Author:Svilen Gospodinov

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -68232,7 +68232,7 @@ catch(err) {
 if (runningTests) {
   require("frontend-cp/tests/test-helper");
 } else {
-  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+cd2ce461"});
+  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+63f5bd46"});
 }
 
 /* jshint ignore:end */
