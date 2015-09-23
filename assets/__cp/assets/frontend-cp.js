6dd23c9816734bec39d474924803e585060cafe3
Travis CI <travis@kayako.com>
2015-09-23 08:46:41 +0000

    Merge pull request #639 from RuslanZavacky/novo-279-case-views-order-agent-area
    NOVO-279 Bugfix for views sorting Author:Arthur Comben

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -44557,15 +44557,15 @@ define('frontend-cp/mirage/fixtures/en-us-strings', ['exports'], function (expor
       'resource_type': 'locale_string'
     }, {
       id: 'frontend.api.admin.predicate_builder.operators.collection_contains_insensitive',
-      value: 'String contains (case insensitive)',
+      value: 'contains (case insensitive)',
       'resource_type': 'locale_string'
     }, {
       id: 'frontend.api.admin.predicate_builder.operators.collection_does_not_contain_insensitive',
-      value: 'String does not contain (case insensitive)',
+      value: 'does not contain (case insensitive)',
       'resource_type': 'locale_string'
     }, {
       id: 'frontend.api.admin.predicate_builder.operators.collection_contains_any_insensitive',
-      value: 'String contains any (case insensitive)',
+      value: 'contains any (case insensitive)',
       'resource_type': 'locale_string'
     }, {
       id: 'frontend.api.cases.activity',
@@ -76577,7 +76577,7 @@ catch(err) {
 if (runningTests) {
   require("frontend-cp/tests/test-helper");
 } else {
-  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+eff1ba44"});
+  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"key":"a092caf2ca262a318f02"},"name":"frontend-cp","version":"0.0.0+51cbb2b6"});
 }
 
 /* jshint ignore:end */
