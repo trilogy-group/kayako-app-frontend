0e35820be36567912ac47cb2396ff09b18b67cee
Travis CI <travis@kayako.com>
2015-11-05 15:13:40 +0000

    Merge pull request #805 from stuartquin/FT-214-people-dropdown-styles
    FT-214 Fix diamond overlay, add pointer cursor Author:Matteo Depalo

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -81218,7 +81218,7 @@ catch(err) {
 if (runningTests) {
   require("frontend-cp/tests/test-helper");
 } else {
-  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"encrypted":true,"key":"88d34fd0054d469bcfa2","authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"name":"frontend-cp","version":"0.0.0+ea03d3f7"});
+  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"logEvents":false,"encrypted":true,"key":"88d34fd0054d469bcfa2","authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"name":"frontend-cp","version":"0.0.0+cb818fa5"});
 }
 
 /* jshint ignore:end */
