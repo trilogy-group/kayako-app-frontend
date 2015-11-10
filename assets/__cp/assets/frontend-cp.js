f27e81a978fd9fd7eecfae31c51a448f872c1bdf
Travis CI <travis@kayako.com>
2015-11-10 14:02:46 +0000

    Merge pull request #834 from stuartquin/fix/reply-with-quote-bug
    Fix a regression introduced recently Author:Miguel Camba

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -81121,7 +81121,7 @@ catch(err) {
 if (runningTests) {
   require("frontend-cp/tests/test-helper");
 } else {
-  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"disabled":false,"logEvents":false,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"name":"frontend-cp","version":"0.0.0+e7695477"});
+  require("frontend-cp/app")["default"].create({"PUSHER_OPTIONS":{"disabled":false,"logEvents":false,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"name":"frontend-cp","version":"0.0.0+b53bbe29"});
 }
 
 /* jshint ignore:end */
