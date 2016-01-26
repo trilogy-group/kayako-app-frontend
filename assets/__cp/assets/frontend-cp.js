f3c8347e0e31e09e51950f45e2b0643d8e5cfd95
Travis CI <travis@kayako.com>
2016-01-26 13:30:13 +0000

    Merge pull request #1106 from cibernox/update_ember_power_select_to_0.8.3
    FT-414 Update ember-power-select to 0.8.3 Author:The Game (develop)

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -66552,6 +66552,6 @@ catch(err) {
 
 /* jshint ignore:start */
 if (!runningTests) {
-  require("frontend-cp/app")["default"].create({"autodismissTimeout":3000,"PUSHER_OPTIONS":{"disabled":false,"logEvents":true,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"views":{"maxLimit":999,"viewsPollingInterval":30,"isPollingEnabled":true},"name":"frontend-cp","version":"0.0.0+46ee14db"});
+  require("frontend-cp/app")["default"].create({"autodismissTimeout":3000,"PUSHER_OPTIONS":{"disabled":false,"logEvents":true,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"views":{"maxLimit":999,"viewsPollingInterval":30,"isPollingEnabled":true},"name":"frontend-cp","version":"0.0.0+8bb564fe"});
 }
 /* jshint ignore:end */
