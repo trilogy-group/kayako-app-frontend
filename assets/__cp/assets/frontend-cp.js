f1967a192dd5072aab68ff95f9cacde5d1d0704b
Travis CI <travis@kayako.com>
2016-03-02 16:08:49 +0000

    Merge pull request #1193 from chrisgame/FT-499-chosen-file-caption
    FT-499 fix file caption after upload Author:Ruslan Zavacky (develop)

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -31065,7 +31065,6 @@ define('frontend-cp/components/ko-file-field/component', ['exports', 'ember'], f
     change: function change() {
       var files = this.element.files;
       this.sendAction('on-change', files);
-      this.element.value = null;
     }
   });
 });
@@ -82799,6 +82798,6 @@ catch(err) {
 
 /* jshint ignore:start */
 if (!runningTests) {
-  require("frontend-cp/app")["default"].create({"autodismissTimeout":3000,"PUSHER_OPTIONS":{"disabled":false,"logEvents":true,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"views":{"maxLimit":999,"viewsPollingInterval":30,"casesPollingInterval":30,"isPollingEnabled":true},"name":"frontend-cp","version":"0.0.0+670ad8b6"});
+  require("frontend-cp/app")["default"].create({"autodismissTimeout":3000,"PUSHER_OPTIONS":{"disabled":false,"logEvents":true,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"views":{"maxLimit":999,"viewsPollingInterval":30,"casesPollingInterval":30,"isPollingEnabled":true},"name":"frontend-cp","version":"0.0.0+dd432ae4"});
 }
 /* jshint ignore:end */
