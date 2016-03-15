6b8718157425bb81c5d80ec39c39edbfdbf0a9eb
Travis CI <travis@kayako.com>
2016-03-15 09:24:17 +0000

    Merge pull request #1220 from kayako-utsavhanda/Feature/FT-523
    FT-523 Author:Ruslan Zavacky (develop)

--- assets/__cp/assets/frontend-cp.js
+++ assets/__cp/assets/frontend-cp.js
@@ -99786,7 +99786,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("frontend-cp/app")["default"].create({"autodismissTimeout":3000,"PUSHER_OPTIONS":{"disabled":false,"logEvents":true,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"views":{"maxLimit":999,"viewsPollingInterval":30,"casesPollingInterval":30,"isPollingEnabled":true},"name":"frontend-cp","version":"0.0.0+53b02a37"});
+  require("frontend-cp/app")["default"].create({"autodismissTimeout":3000,"PUSHER_OPTIONS":{"disabled":false,"logEvents":true,"encrypted":true,"authEndpoint":"/api/v1/realtime/auth","wsHost":"ws.realtime.kayako.com","httpHost":"sockjs.realtime.kayako.com"},"views":{"maxLimit":999,"viewsPollingInterval":30,"casesPollingInterval":30,"isPollingEnabled":true},"name":"frontend-cp","version":"0.0.0+fedffecc"});
 }
 
 /* jshint ignore:end */
