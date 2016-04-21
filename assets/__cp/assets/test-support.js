acb249254e760987c7b75e8c170f53a28cebc3a7
Travis CI <travis@kayako.com>
2016-04-21 17:10:36 +0000

    Merge pull request #1278 from kayako/sass-fix
    Fix build failing due to sass extend Author:Miguel Camba (develop)
    
    
    Former-commit-id: 5e2bccc39cad0ba77ff16e4c939c60dbcbb5029c
    Former-commit-id: 86319e079303666b5b43a8481474871bd527ee0e [formerly 0cbe54c869d6b5d8eb62d94d97f5048eeb7c6443]
    Former-commit-id: 2db3f89ed0a5d2c2fcefe202765276b0e0f9cde0

--- assets/__cp/assets/test-support.js
+++ assets/__cp/assets/test-support.js
@@ -1872,14 +1872,14 @@ requireModule("ember-testing");
 }());
 
 /*!
- * QUnit 1.23.0
+ * QUnit 1.23.1
  * https://qunitjs.com/
  *
  * Copyright jQuery Foundation and other contributors
  * Released under the MIT license
  * https://jquery.org/license
  *
- * Date: 2016-03-25T19:37Z
+ * Date: 2016-04-12T17:29Z
  */
 
 ( function( global ) {
@@ -2257,7 +2257,7 @@ function verifyLoggingCallbacks() {
 QUnit.isLocal = !( defined.document && window.location.protocol !== "file:" );
 
 // Expose the current QUnit version
-QUnit.version = "1.23.0";
+QUnit.version = "1.23.1";
 
 extend( QUnit, {
 
@@ -2267,7 +2267,7 @@ extend( QUnit, {
 		var currentModule = config.currentModule;
 
 		if ( arguments.length === 2 ) {
-			if ( testEnvironment instanceof Function ) {
+			if ( objectType( testEnvironment ) === "function" ) {
 				executeNow = testEnvironment;
 				testEnvironment = undefined;
 			}
@@ -2291,7 +2291,7 @@ extend( QUnit, {
 			afterEach: setHook( module, "afterEach" )
 		};
 
-		if ( executeNow instanceof Function ) {
+		if ( objectType( executeNow ) === "function" ) {
 			config.moduleStack.push( module );
 			setCurrentModule( module );
 			executeNow.call( module.testEnvironment, moduleFns );
@@ -3497,7 +3497,7 @@ QUnit.assert = Assert.prototype = {
 // Known to us are: Closure Compiler, Narwhal
 ( function() {
 	/*jshint sub:true */
-	Assert.prototype.raises = Assert.prototype.throws;
+	Assert.prototype.raises = Assert.prototype [ "throws" ]; //jscs:ignore requireDotNotation
 }() );
 
 function errorString( error ) {
