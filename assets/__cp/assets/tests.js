4cebfd2c3b3979b48e3f6cc87742b6c950d10eeb
Travis CI <travis@kayako.com>
2016-04-28 07:54:10 +0000

    Merge pull request #1290 from chrisgame/FT-580-rename-to-triggers
    FT-580 rename case-triggers to triggers Author:Jamie White (develop)

--- assets/__cp/assets/tests.js
+++ assets/__cp/assets/tests.js
@@ -70,173 +70,6 @@ define('frontend-cp/tests/acceptance/admin/automation/businesshours/edit-test', 
     });
   });
 });
-define('frontend-cp/tests/acceptance/admin/automation/case-triggers/index-test', ['exports', 'frontend-cp/tests/helpers/qunit', 'frontend-cp/components/ko-simple-list/row/styles'], function (exports, _frontendCpTestsHelpersQunit, _frontendCpComponentsKoSimpleListRowStyles) {
-
-  var originalConfirm = undefined;
-
-  (0, _frontendCpTestsHelpersQunit.app)('Acceptance | admin/automation/case-triggers/index', {
-    beforeEach: function beforeEach() {
-      var locale = server.create('locale', {
-        id: 1,
-        locale: 'en-us'
-      });
-
-      var adminRole = server.create('role', { type: 'ADMIN' });
-      var agent = server.create('user', { role: adminRole, locale: locale });
-      var session = server.create('session', { user: agent });
-      login(session.id);
-
-      server.create('plan', {
-        limits: [],
-        features: []
-      });
-      originalConfirm = window.confirm;
-    },
-
-    afterEach: function afterEach() {
-      window.confirm = originalConfirm;
-      logout();
-    }
-  });
-
-  (0, _frontendCpTestsHelpersQunit.test)('deleting an enabled trigger field', function (assert) {
-    assert.expect(4);
-
-    server.create('trigger', { title: 'test trigger', is_enabled: true, execution_order: 1 });
-
-    visit('/admin/automation/case-triggers');
-
-    window.confirm = function (message) {
-      assert.equal(message, 'Are you sure you want to delete this?', 'The proper confirm message is shown');
-      return true;
-    };
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      triggerEvent('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger")', 'mouseenter');
-    });
-
-    andThen(function () {
-      click('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Delete)');
-    });
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.notOk(find('span:contains("test trigger")').length > 0);
-    });
-  });
-
-  (0, _frontendCpTestsHelpersQunit.test)('disabled triggers cannot be deleted', function (assert) {
-    assert.expect(2);
-
-    server.create('trigger', { title: 'test trigger', is_enabled: false, execution_order: 1 });
-
-    visit('/admin/automation/case-triggers');
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.notOk(find('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Delete)').length > 0);
-    });
-  });
-
-  (0, _frontendCpTestsHelpersQunit.test)('disabled triggers can be enabled', function (assert) {
-    assert.expect(6);
-
-    server.create('trigger', { title: 'test trigger', is_enabled: false, execution_order: 1 });
-
-    visit('/admin/automation/case-triggers');
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.ok(find('.qa-admin_case-tiggers--enabled li').length === 0);
-      assert.ok(find('.qa-admin_case-tiggers--disabled').length === 1);
-      triggerEvent('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger")', 'mouseenter');
-      click('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Enable)');
-    });
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.ok(find('.qa-admin_case-tiggers--enabled li').length === 1);
-      assert.ok(find('.qa-admin_case-tiggers--disabled').length === 0);
-    });
-  });
-
-  (0, _frontendCpTestsHelpersQunit.test)('enabled triggers can be disabled', function (assert) {
-    assert.expect(6);
-
-    server.create('trigger', { title: 'test trigger', is_enabled: true, execution_order: 1 });
-
-    visit('/admin/automation/case-triggers');
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.ok(find('.qa-admin_case-tiggers--enabled li').length === 1);
-      assert.ok(find('.qa-admin_case-tiggers--disabled').length === 0);
-      triggerEvent('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger")', 'mouseenter');
-      click('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Disable)');
-    });
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.ok(find('.qa-admin_case-tiggers--enabled li').length === 0);
-      assert.ok(find('.qa-admin_case-tiggers--disabled').length === 1);
-    });
-  });
-
-  (0, _frontendCpTestsHelpersQunit.test)('triggers are ordered by their execution order and then grouped by enabled and disabled', function (assert) {
-    assert.expect(6);
-
-    server.create('trigger', { title: 'test trigger 5', is_enabled: false, execution_order: 5 });
-    server.create('trigger', { title: 'test trigger 4', is_enabled: true, execution_order: 4 });
-    server.create('trigger', { title: 'test trigger 3', is_enabled: true, execution_order: 3 });
-    server.create('trigger', { title: 'test trigger 2', is_enabled: true, execution_order: 2 });
-    server.create('trigger', { title: 'test trigger 1', is_enabled: false, execution_order: 1 });
-
-    visit('/admin/automation/case-triggers');
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(1) .t-bold').text().trim(), 'test trigger 2');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(2) .t-bold').text().trim(), 'test trigger 3');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(3) .t-bold').text().trim(), 'test trigger 4');
-      assert.equal(find('.qa-admin_case-tiggers--disabled .' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':eq(0) .t-bold').text().trim(), 'test trigger 1');
-      assert.equal(find('.qa-admin_case-tiggers--disabled .' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':eq(1) .t-bold').text().trim(), 'test trigger 5');
-    });
-  });
-
-  (0, _frontendCpTestsHelpersQunit.test)('enabled triggers can be reordered', function (assert) {
-    assert.expect(11);
-
-    server.create('trigger', { title: 'test trigger 1', is_enabled: true, execution_order: 1 });
-    server.create('trigger', { title: 'test trigger 2', is_enabled: true, execution_order: 2 });
-    server.create('trigger', { title: 'test trigger 3', is_enabled: true, execution_order: 3 });
-    server.create('trigger', { title: 'test trigger 4', is_enabled: true, execution_order: 4 });
-    server.create('trigger', { title: 'test trigger 5', is_enabled: true, execution_order: 5 });
-
-    visit('/admin/automation/case-triggers');
-
-    scrollToBottomOfPage();
-
-    andThen(function () {
-      assert.equal(currentURL(), '/admin/automation/case-triggers');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(1) .t-bold').text().trim(), 'test trigger 1');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(2) .t-bold').text().trim(), 'test trigger 2');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(3) .t-bold').text().trim(), 'test trigger 3');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(4) .t-bold').text().trim(), 'test trigger 4');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(5) .t-bold').text().trim(), 'test trigger 5');
-    });
-
-    reorderListItems('.i-dragstrip', '.t-bold', 'test trigger 5', 'test trigger 4', 'test trigger 3', 'test trigger 2', 'test trigger 1');
-
-    andThen(function () {
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(1) .t-bold').text().trim(), 'test trigger 5');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(2) .t-bold').text().trim(), 'test trigger 4');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(3) .t-bold').text().trim(), 'test trigger 3');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(4) .t-bold').text().trim(), 'test trigger 2');
-      assert.equal(find('.qa-admin_case-tiggers--enabled li:nth-of-type(5) .t-bold').text().trim(), 'test trigger 1');
-    });
-  });
-});
 define('frontend-cp/tests/acceptance/admin/automation/monitors/edit-test', ['exports', 'frontend-cp/tests/helpers/qunit', 'frontend-cp/components/ko-simple-list/row/styles'], function (exports, _frontendCpTestsHelpersQunit, _frontendCpComponentsKoSimpleListRowStyles) {
 
   var originalConfirm = undefined;
@@ -1011,6 +844,173 @@ define('frontend-cp/tests/acceptance/admin/automation/monitors/new-test', ['expo
 
     andThen(function () {
       assert.equal(currentURL(), '/admin/automation/monitors');
+    });
+  });
+});
+define('frontend-cp/tests/acceptance/admin/automation/triggers/index-test', ['exports', 'frontend-cp/tests/helpers/qunit', 'frontend-cp/components/ko-simple-list/row/styles'], function (exports, _frontendCpTestsHelpersQunit, _frontendCpComponentsKoSimpleListRowStyles) {
+
+  var originalConfirm = undefined;
+
+  (0, _frontendCpTestsHelpersQunit.app)('Acceptance | admin/automation/triggers/index', {
+    beforeEach: function beforeEach() {
+      var locale = server.create('locale', {
+        id: 1,
+        locale: 'en-us'
+      });
+
+      var adminRole = server.create('role', { type: 'ADMIN' });
+      var agent = server.create('user', { role: adminRole, locale: locale });
+      var session = server.create('session', { user: agent });
+      login(session.id);
+
+      server.create('plan', {
+        limits: [],
+        features: []
+      });
+      originalConfirm = window.confirm;
+    },
+
+    afterEach: function afterEach() {
+      window.confirm = originalConfirm;
+      logout();
+    }
+  });
+
+  (0, _frontendCpTestsHelpersQunit.test)('deleting an enabled trigger field', function (assert) {
+    assert.expect(4);
+
+    server.create('trigger', { title: 'test trigger', is_enabled: true, execution_order: 1 });
+
+    visit('/admin/automation/triggers');
+
+    window.confirm = function (message) {
+      assert.equal(message, 'Are you sure you want to delete this?', 'The proper confirm message is shown');
+      return true;
+    };
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      triggerEvent('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger")', 'mouseenter');
+    });
+
+    andThen(function () {
+      click('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Delete)');
+    });
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.notOk(find('span:contains("test trigger")').length > 0);
+    });
+  });
+
+  (0, _frontendCpTestsHelpersQunit.test)('disabled triggers cannot be deleted', function (assert) {
+    assert.expect(2);
+
+    server.create('trigger', { title: 'test trigger', is_enabled: false, execution_order: 1 });
+
+    visit('/admin/automation/triggers');
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.notOk(find('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Delete)').length > 0);
+    });
+  });
+
+  (0, _frontendCpTestsHelpersQunit.test)('disabled triggers can be enabled', function (assert) {
+    assert.expect(6);
+
+    server.create('trigger', { title: 'test trigger', is_enabled: false, execution_order: 1 });
+
+    visit('/admin/automation/triggers');
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.ok(find('.qa-admin_triggers--enabled li').length === 0);
+      assert.ok(find('.qa-admin_triggers--disabled').length === 1);
+      triggerEvent('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger")', 'mouseenter');
+      click('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Enable)');
+    });
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.ok(find('.qa-admin_triggers--enabled li').length === 1);
+      assert.ok(find('.qa-admin_triggers--disabled').length === 0);
+    });
+  });
+
+  (0, _frontendCpTestsHelpersQunit.test)('enabled triggers can be disabled', function (assert) {
+    assert.expect(6);
+
+    server.create('trigger', { title: 'test trigger', is_enabled: true, execution_order: 1 });
+
+    visit('/admin/automation/triggers');
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.ok(find('.qa-admin_triggers--enabled li').length === 1);
+      assert.ok(find('.qa-admin_triggers--disabled').length === 0);
+      triggerEvent('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger")', 'mouseenter');
+      click('.' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':contains("test trigger") a:contains(Disable)');
+    });
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.ok(find('.qa-admin_triggers--enabled li').length === 0);
+      assert.ok(find('.qa-admin_triggers--disabled').length === 1);
+    });
+  });
+
+  (0, _frontendCpTestsHelpersQunit.test)('triggers are ordered by their execution order and then grouped by enabled and disabled', function (assert) {
+    assert.expect(6);
+
+    server.create('trigger', { title: 'test trigger 5', is_enabled: false, execution_order: 5 });
+    server.create('trigger', { title: 'test trigger 4', is_enabled: true, execution_order: 4 });
+    server.create('trigger', { title: 'test trigger 3', is_enabled: true, execution_order: 3 });
+    server.create('trigger', { title: 'test trigger 2', is_enabled: true, execution_order: 2 });
+    server.create('trigger', { title: 'test trigger 1', is_enabled: false, execution_order: 1 });
+
+    visit('/admin/automation/triggers');
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(1) .t-bold').text().trim(), 'test trigger 2');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(2) .t-bold').text().trim(), 'test trigger 3');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(3) .t-bold').text().trim(), 'test trigger 4');
+      assert.equal(find('.qa-admin_triggers--disabled .' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':eq(0) .t-bold').text().trim(), 'test trigger 1');
+      assert.equal(find('.qa-admin_triggers--disabled .' + _frontendCpComponentsKoSimpleListRowStyles['default'].row + ':eq(1) .t-bold').text().trim(), 'test trigger 5');
+    });
+  });
+
+  (0, _frontendCpTestsHelpersQunit.test)('enabled triggers can be reordered', function (assert) {
+    assert.expect(11);
+
+    server.create('trigger', { title: 'test trigger 1', is_enabled: true, execution_order: 1 });
+    server.create('trigger', { title: 'test trigger 2', is_enabled: true, execution_order: 2 });
+    server.create('trigger', { title: 'test trigger 3', is_enabled: true, execution_order: 3 });
+    server.create('trigger', { title: 'test trigger 4', is_enabled: true, execution_order: 4 });
+    server.create('trigger', { title: 'test trigger 5', is_enabled: true, execution_order: 5 });
+
+    visit('/admin/automation/triggers');
+
+    scrollToBottomOfPage();
+
+    andThen(function () {
+      assert.equal(currentURL(), '/admin/automation/triggers');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(1) .t-bold').text().trim(), 'test trigger 1');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(2) .t-bold').text().trim(), 'test trigger 2');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(3) .t-bold').text().trim(), 'test trigger 3');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(4) .t-bold').text().trim(), 'test trigger 4');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(5) .t-bold').text().trim(), 'test trigger 5');
+    });
+
+    reorderListItems('.i-dragstrip', '.t-bold', 'test trigger 5', 'test trigger 4', 'test trigger 3', 'test trigger 2', 'test trigger 1');
+
+    andThen(function () {
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(1) .t-bold').text().trim(), 'test trigger 5');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(2) .t-bold').text().trim(), 'test trigger 4');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(3) .t-bold').text().trim(), 'test trigger 3');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(4) .t-bold').text().trim(), 'test trigger 2');
+      assert.equal(find('.qa-admin_triggers--enabled li:nth-of-type(5) .t-bold').text().trim(), 'test trigger 1');
     });
   });
 });
