cd8bd9ab745a3fae5339dd2093193f5dd66267c3
Travis CI <travis@kayako.com>
2016-06-29 12:06:23 +0000

    Merge pull request #1581 from chrisgame/FT-913-delete-clock-tests
    FT-913 always going to be brittle, removing Author:Jamie White (develop)

--- assets/__cp/assets/tests.js
+++ assets/__cp/assets/tests.js
@@ -20675,40 +20675,6 @@ define('frontend-cp/tests/unit/services/plan-test', ['exports', 'ember', 'ember-
     });
   });
 });
-define('frontend-cp/tests/unit/services/server-clock-test', ['exports', 'ember-qunit', 'moment'], function (exports, _emberQunit, _moment) {
-
-  var serverClock = undefined;
-  (0, _emberQunit.moduleFor)('service:server-clock', 'Unit | Service | server clock', {
-    needs: ['service:server-clock'],
-    beforeEach: function beforeEach() {
-      serverClock = this.subject();
-    }
-  });
-
-  (0, _emberQunit.test)('it handles lastKnownServerTime not being set', function (assert) {
-    assert.equal(serverClock.get('skew'), 0, 'skew should be 0');
-  });
-
-  (0, _emberQunit.test)('it calculates the diff between now and the future', function (assert) {
-    var future = (0, _moment['default'])().add(1, 'minutes');
-
-    serverClock.set('lastKnownServerTime', future);
-    assert.ok(serverClock.get('skew') > 59900, 'positive diff');
-  });
-
-  (0, _emberQunit.test)('it calculates the diff between now and the past', function (assert) {
-    var future = (0, _moment['default'])().subtract(1, 'minutes');
-
-    serverClock.set('lastKnownServerTime', future);
-    assert.ok(serverClock.get('skew') < -60000, 'negative diff');
-  });
-
-  (0, _emberQunit.test)('it returns current date if the date passed is null', function (assert) {
-    var now = (0, _moment['default'])();
-
-    assert.equal(serverClock.applySkew(null).format('lll'), now.format('lll'), 'current date time');
-  });
-});
 define('frontend-cp/tests/unit/utils/promise-queue-test', ['exports', 'ember', 'qunit', 'ember-qunit', 'frontend-cp/utils/promise-queue'], function (exports, _ember, _qunit, _emberQunit, _frontendCpUtilsPromiseQueue) {
 
   (0, _qunit.module)('Unit | Utils | promise-queue');
