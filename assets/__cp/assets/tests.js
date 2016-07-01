19ac67cf7e1b59183e817bc433416dc58f765015
Travis CI <travis@kayako.com>
2016-07-01 12:20:47 +0000

    Merge pull request #1604 from thetutlage/feature/trial-improvement
    FT-929 Add validation service to incooperate new validations Author:Jamie White (develop)

--- assets/__cp/assets/tests.js
+++ assets/__cp/assets/tests.js
@@ -20885,6 +20885,151 @@ define('frontend-cp/tests/unit/utils/promise-queue-test', ['exports', 'ember', '
     assert.equal(0, queue.queue.length);
   });
 });
+define('frontend-cp/tests/unit/validation/service-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('service:validation', 'Unit | Service | validation', {
+    // Specify the other units that are required for this test.
+    // needs: ['service:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('return false when value does not exists', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.exists(undefined), false);
+  });
+
+  (0, _emberQunit.test)('return false when value is an empty string', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.exists(''), false);
+  });
+
+  (0, _emberQunit.test)('return false when value is null', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.exists(null), false);
+  });
+
+  (0, _emberQunit.test)('return true when value does exists', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.exists('foo'), true);
+  });
+
+  (0, _emberQunit.test)('return false when value is not an email address', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.isEmail('foo'), false);
+  });
+
+  (0, _emberQunit.test)('return true when value is an email address', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.isEmail('foo@bar.com'), true);
+  });
+
+  (0, _emberQunit.test)('return false when value is not a phone number', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.isPhone('9192abc'), false);
+  });
+
+  (0, _emberQunit.test)('return true when value is a phone number', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.isPhone('9990345109'), true);
+  });
+
+  (0, _emberQunit.test)('return true when value is a phone number with dashes', function (assert) {
+    assert.expect(1);
+
+    var service = this.subject();
+    assert.equal(service.isPhone('999-034-5109'), true);
+  });
+
+  (0, _emberQunit.test)('return object of errors from validateAll when validation fails', function (assert) {
+    assert.expect(1);
+    var data = {
+      name: '',
+      email: 'foo'
+    };
+
+    var rules = {
+      name: ['exists'],
+      email: ['exists', 'isEmail']
+    };
+
+    var messages = {
+      name: 'name is required',
+      email: 'enter valid email address'
+    };
+
+    var expected = {
+      name: [{ message: 'name is required' }],
+      email: [{ message: 'enter valid email address' }]
+    };
+
+    var service = this.subject();
+    assert.deepEqual(service.validateAll(data, rules, messages), expected);
+  });
+
+  (0, _emberQunit.test)('return object of errors from validateAll when validation fails on single field', function (assert) {
+    assert.expect(1);
+    var data = {
+      name: 'foo',
+      email: 'foo'
+    };
+
+    var rules = {
+      name: ['exists'],
+      email: ['exists', 'isEmail']
+    };
+
+    var messages = {
+      name: 'name is required',
+      email: 'enter valid email address'
+    };
+
+    var expected = {
+      email: [{ message: 'enter valid email address' }]
+    };
+
+    var service = this.subject();
+    assert.deepEqual(service.validateAll(data, rules, messages), expected);
+  });
+
+  (0, _emberQunit.test)('return empty object when everything is good', function (assert) {
+    assert.expect(1);
+    var data = {
+      name: 'foo',
+      email: 'foo@bar.com'
+    };
+
+    var rules = {
+      name: ['exists'],
+      email: ['exists', 'isEmail']
+    };
+
+    var messages = {
+      name: 'name is required',
+      email: 'enter valid email address'
+    };
+
+    var expected = {};
+
+    var service = this.subject();
+    assert.deepEqual(service.validateAll(data, rules, messages), expected);
+  });
+});
 /* jshint ignore:start */
 
 require('frontend-cp/tests/test-helper');
