8205e8878e6e5c49a5ee2b19ecacc34711ed21cc
Loe Lobo <loe.lobo@crossover.com>
2018-04-27 18:05:42 +0000

    Small typo fix  Author:Loe Lobo (develop)

--- assets/__cp/service-worker.js
+++ assets/__cp/service-worker.js
@@ -1,2 +1,6 @@
-function makeCaseUrl(t){return t.currentTarget.origin+"/agent/cases/"+t.notification.data.id}function getActionsForNotification(t){return"case"===t.resource_type&&t.resource_id?[{action:"open-case",title:"Open case"}]:[]}self.addEventListener("notificationclick",function(t){if(""===t.action||"case"===t.notification.type){var i=makeCaseUrl(t);clients.openWindow(i)}}),self.addEventListener("install",function(t){t.waitUntil(self.skipWaiting())}),self.addEventListener("push",function(t){if(t.data){var i=t.data.json();if(i.title){var e=self.registration.showNotification(i.title,{body:i.summary,icon:i.avatar_url,data:{id:i.resource_id},type:i.resource_type});t.waitUntil(e)}}});
+function makeCaseUrl(t){return t.currentTarget.origin+"/agent/conversations/"+t.notification.data.id}function getActionsForNotification(t){return"case"===t.resource_type&&t.resource_id?[{action:"open-case",title:"Open conversation"}]:[]}self.addEventListener("notificationclick",function(t){if(""===t.action||"case"===t.notification.type){var i=makeCaseUrl(t)
+clients.openWindow(i)}}),self.addEventListener("install",function(t){t.waitUntil(self.skipWaiting())}),self.addEventListener("push",function(t){if(t.data){var i=t.data.json()
+if(i.title){var e=self.registration.showNotification(i.title,{body:i.summary,icon:i.avatar_url,data:{id:i.resource_id},type:i.resource_type})
+t.waitUntil(e)}}})
+
 //# sourceMappingURL=service-worker.map
